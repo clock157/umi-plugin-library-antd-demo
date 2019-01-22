1588f75bb08d105e217b68f662cfb9c1159d1a49
clock157 <clock157@163.com>
2019-01-22 12:35:55 +0800

    feat: 调整

--- .umirc.js
+++ .umirc.js
@@ -6,26 +6,6 @@ export default {
         doc: {
           theme: "docz-theme-antd"
         },
-        input: "src/index.js",
-        esm: {
-          type: "babel",    // rollup babel
-        },
-        cjs: {
-          type: "rollup"  // rollup babel
-        },
-        umd: {
-          name: 'umiPluginLibraryDemo', // default camel case package name
-          global: {
-            'react': 'React'
-          }
-        },
-        namedExports: {
-          [require.resolve("react-is")]: [
-            "ForwardRef",
-            "isElement",
-            "isValidElementType"
-          ]
-        }
       }
     ]
   ]
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // @create-index
 
-export { default as AcpAlert } from './AcpAlert';
 export { default as AvatarList } from './AvatarList';
 export { default as CountDown } from './CountDown';
 
